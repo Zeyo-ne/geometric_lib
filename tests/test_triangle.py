6ccf05d3c7b9ac2c83b544470d402c58165a3c80
Zeyo-ne <sazhin2007.ru@gmail.com>
2025-12-22 22:45:58 +0300

    Add CI workflow for running unit tests and some edits of files done

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -10,20 +10,3 @@ class TriangleTestCase(unittest.TestCase):
 
     def test_perimeter_3_4_5(self):
         self.assertEqual(perimeter(3, 4, 5), 12)
-
-    def test_area_equilateral(self):
-        # равносторонний треугольник
-        a = 10
-        expected_area = (a**2 * 3**0.5) / 4
-        self.assertAlmostEqual(area(a, a, a), expected_area)
-
-    def test_perimeter_equilateral(self):
-        self.assertEqual(perimeter(5, 5, 5), 15)
-
-    def test_area_float_values(self):
-        self.assertAlmostEqual(area(2.5, 3.5, 4), 4.353, places=3)
-
-    def test_big_numbers(self):
-        a = 10**6
-        expected_area = (a**2 * 3**0.5) / 4
-        self.assertAlmostEqual(area(a, a, a), expected_area)
